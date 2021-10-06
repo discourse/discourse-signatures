deca01facbe7e68cb9e8c2bbf6ddbf3375ddbfa5
Simon Cossar <scossar3@gmail.com>
2021-10-06 16:53:59 -0700

    Update the about field (#15)

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # name: discourse-signatures
-# about: A plugin to get that nostalgia signatures in Discourse Foruns
+# about: Adds signatures to Discourse posts
 # version: 2.0.0
 # author: Rafael Silva <xfalcox@gmail.com>
 # url: https://github.com/discourse/discourse-signatures
