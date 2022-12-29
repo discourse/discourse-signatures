3cd87fbd927fce7ccde625ce39d9f7cd9fd3a222
David Taylor <david@taylorhq.com>
2022-12-29 10:24:15 +0000

    DEV: Introduce syntax_tree for ruby formatting

--- plugin.rb
+++ plugin.rb
@@ -14,43 +14,46 @@ DiscoursePluginRegistry.serialized_current_user_fields << "signature_url"
 DiscoursePluginRegistry.serialized_current_user_fields << "signature_raw"
 
 after_initialize do
-  User.register_custom_field_type('see_signatures', :boolean)
-  User.register_custom_field_type('signature_url', :text)
-  User.register_custom_field_type('signature_raw', :text)
+  User.register_custom_field_type("see_signatures", :boolean)
+  User.register_custom_field_type("signature_url", :text)
+  User.register_custom_field_type("signature_raw", :text)
 
   # add to class and serializer to allow for default value for the setting
   add_to_class(:user, :see_signatures) do
-    if custom_fields['see_signatures'] != nil
-      custom_fields['see_signatures']
+    if custom_fields["see_signatures"] != nil
+      custom_fields["see_signatures"]
     else
       SiteSetting.signatures_visible_by_default
     end
   end
 
-  add_to_serializer(:user, :see_signatures) do
-    object.see_signatures
-  end
+  add_to_serializer(:user, :see_signatures) { object.see_signatures }
 
-  register_editable_user_custom_field [:see_signatures, :signature_url, :signature_raw]
+  register_editable_user_custom_field %i[see_signatures signature_url signature_raw]
 
   allow_public_user_custom_field :signature_cooked
   allow_public_user_custom_field :signature_url
 
   add_to_serializer(:post, :user_signature) do
     if SiteSetting.signatures_advanced_mode
-      object.user.custom_fields['signature_cooked'] if object.user
+      object.user.custom_fields["signature_cooked"] if object.user
     else
-      object.user.custom_fields['signature_url'] if object.user
+      object.user.custom_fields["signature_url"] if object.user
     end
   end
 
   # This is the code responsible for cooking a new advanced mode sig on user update
   DiscourseEvent.on(:user_updated) do |user|
-    if SiteSetting.signatures_enabled? && SiteSetting.signatures_advanced_mode && user.custom_fields['signature_raw']
-      cooked_sig = PrettyText.cook(user.custom_fields['signature_raw'], omit_nofollow: user.has_trust_level?(TrustLevel[3]) && !SiteSetting.tl3_links_no_follow)
+    if SiteSetting.signatures_enabled? && SiteSetting.signatures_advanced_mode &&
+         user.custom_fields["signature_raw"]
+      cooked_sig =
+        PrettyText.cook(
+          user.custom_fields["signature_raw"],
+          omit_nofollow: user.has_trust_level?(TrustLevel[3]) && !SiteSetting.tl3_links_no_follow,
+        )
       # avoid infinite recursion
-      if cooked_sig != user.custom_fields['signature_cooked']
-        user.custom_fields['signature_cooked'] = cooked_sig
+      if cooked_sig != user.custom_fields["signature_cooked"]
+        user.custom_fields["signature_cooked"] = cooked_sig
         user.save
       end
     end
